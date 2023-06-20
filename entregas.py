4a9ae97f11d918a04d8a10f3377711fdf56ff545
JuliaStella <julia.paula@liferay.com>
2023-06-20 10:04:14 -0300

    Adição da leitura do arquivo

--- entregas.py
+++ entregas.py
@@ -1,5 +1,11 @@
-print("HEllO WORD")
+arquivo = open('matriz', 'r')
 
-def soma(a, b):
-    somac = 0
-  return soma
+print(arquivo)
+
+n, m = arquivo.readline().split() # pega o arquivo e divide em linhas e colunas n e m
+linhas = arquivo.read().splitlines()
+
+coordenadas = {}
+pontoDeliver = []
+
+print(coordenadas, pontoDeliver)
