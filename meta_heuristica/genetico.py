2787d9c43b533c00b291da5c124ac27340d87e11
JuliaStella <julia.paula@liferay.com>
2023-08-28 12:43:10 -0300

    Criacao do fitness

--- meta_heuristica/genetico.py
+++ meta_heuristica/genetico.py
@@ -1,6 +1,5 @@
-import matplotlib
-import random
-
+import numpy as random, matplotlib
+from typing import List, Tuple
 
 class Cidade:
     """ Inicia a cidade com duas coordenadas x,y"""
@@ -26,3 +25,26 @@ def popInicial(tam_pop, cidades):
     return populacao
 
 
+def avaliar_rota(rota: List[str], coordenadas: List[tuple[int, int]]) -> float:
+    """ Primeira parte do fitness, calcula todas as coodernadas e retona o custo de cada uma."""
+    custo_total = 0
+    ponto_anterior = 'R' # Ponto de origem.
+    coordenadas_dicionario = {coordenadas} # Converto a minha lista em um dicionario, para o acesso mais rapido
+
+    for ponto_atual in rota: # calculo da distancia de todos os pontos das coordenadas
+      if ponto_atual != ponto_anterior:
+          custo_total += distanciaEuc(coordenadas_dicionario[ponto_anterior], coordenadas_dicionario[ponto_atual])
+    
+    return custo_total
+
+def avaliar_populacao(populacao: List[str], coordenadas: List[tuple[int, int]]) -> list[float]:
+    """Ultima parte do fitness: calcula os valores de fitness para cada rota na população e armazena elas em uma lista"""
+
+
+    fitnes = []
+
+    for rota in populacao:
+        custo_rota = avaliar_rota(rota, coordenadas)
+        fitnes.append(custo_rota)
+    
+    return fitnes
