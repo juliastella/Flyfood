4f0875d10bf79a77083b81a1a4b2bfc3117e6903
juliastella <juliastellasil1@gmai.com>
2023-09-05 23:00:35 -0300

    Atualização do formiga

--- meta_heuristica/colonia_formiga.py
+++ meta_heuristica/colonia_formiga.py
@@ -1,75 +1,87 @@
 import random
-import numpy as np
+import math
 
-# Função para calcular a distância euclidiana entre duas cidades
-def distancia_euclidiana(cidade1, cidade2):
-    return np.linalg.norm(np.array(cidade1) - np.array(cidade2))
+# Defina os pontos de entrega no plano cartesiano
+pontos_entrega = {
+    'A': (1, 1),
+    'B': (3, 2),
+    'C': (2, 4),
+    'D': (0, 4)
+}
 
-# Função para criar uma matriz de feromônios inicializada com um valor pequeno
-def criar_matriz_feromonios(num_cidades):
-    return np.ones((num_cidades, num_cidades)) * 0.01
+# Parâmetros do algoritmo de colônia de formigas
+num_formigas = 100       # Número de formigas
+num_iteracoes = 100      # Número de iterações
+alpha = 1.0              # Peso do feromônio
+beta = 2.0               # Peso da distância
+taxa_evaporacao = 0.1    # Taxa de evaporção do feromônio
 
-# Função para escolher o próximo destino com base em probabilidades
-def escolher_destino(feromonios, visitados, cidade_atual, alpha, beta):
-    nao_visitados = [i for i in range(len(feromonios)) if i not in visitados]
-    probabilidades = []
-    
-    for cidade in nao_visitados:
-        probabilidade = (feromonios[cidade_atual][cidade] ** alpha) * ((1.0 / distancia_euclidiana(cidades[cidade_atual], cidades[cidade])) ** beta)
-        probabilidades.append((cidade, probabilidade))
-    
-    total_probabilidade = sum(prob[1] for prob in probabilidades)
-    probabilidades = [(prob[0], prob[1] / total_probabilidade) for prob in probabilidades]
-    
-    escolha = random.choices(probabilidades, k=1)[0]
-    return escolha[0]
+# Função para calcular a distância euclidiana entre dois pontos
+def calcular_distancia(p1, p2):
+    return math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
 
-# Função para atualizar os feromônios com base nas soluções encontradas
-def atualizar_feromonios(feromonios, trilha, taxa_evaporacao, Q):
-    for i in range(len(trilha) - 1):
-        cidade_atual, proxima_cidade = trilha[i], trilha[i+1]
-        feromonios[cidade_atual][proxima_cidade] = (1 - taxa_evaporacao) * feromonios[cidade_atual][proxima_cidade] + Q
+# Inicialização das trilhas de feromônio
+feromonio = {}
+for ponto1 in pontos_entrega:
+    for ponto2 in pontos_entrega:
+        feromonio[(ponto1, ponto2)] = 1.0
 
-# Função principal do algoritmo ACO
-def aco(num_formigas, num_iteracoes, alpha, beta, taxa_evaporacao, Q):
-    num_cidades = len(cidades)
-    melhor_rota = None
-    melhor_distancia = float('inf')
-    feromonios = criar_matriz_feromonios(num_cidades)
-    
-    for _ in range(num_iteracoes):
-        for formiga in range(num_formigas):
-            cidade_atual = random.randint(0, num_cidades - 1)
-            visitados = [cidade_atual]
-            
-            while len(visitados) < num_cidades:
-                proxima_cidade = escolher_destino(feromonios, visitados, cidade_atual, alpha, beta)
-                visitados.append(proxima_cidade)
-                cidade_atual = proxima_cidade
-            
-            distancia_rota = sum(distancia_euclidiana(cidades[visitados[i]], cities[visitados[i+1]]) for i in range(num_cidades - 1))
-            
-            if distancia_rota < melhor_distancia:
-                melhor_distancia = distancia_rota
-                melhor_rota = visitados[:]
+# Função para calcular a probabilidade de escolher o próximo ponto
+def calcular_probabilidade(ponto_atual, ponto_destino, visitados):
+    probabilidade = 0.0
+    for ponto in pontos_entrega:
+        if ponto not in visitados:
+            probabilidade += (feromonio[(ponto_atual, ponto)] ** alpha) * ((1.0 / calcular_distancia(pontos_entrega[ponto_atual], pontos_entrega[ponto])) ** beta)
+    return probabilidade
+
+# Função para escolher o próximo ponto com base nas probabilidades
+def escolher_proximo_ponto(ponto_atual, visitados):
+    probabilidades = {}
+    for ponto in pontos_entrega:
+        if ponto not in visitados:
+            probabilidade = calcular_probabilidade(ponto_atual, ponto, visitados)
+            probabilidades[ponto] = probabilidade
+
+    total_probabilidade = sum(probabilidades.values())
+    r = random.uniform(0, total_probabilidade)
+    acumulado = 0.0
+
+    for ponto, prob in probabilidades.items():
+        acumulado += prob
+        if acumulado >= r:
+            return ponto
+
+# Função para atualizar as trilhas de feromônio
+def atualizar_feromonio(caminhos, melhor_caminho):
+    for ponto1, ponto2 in caminhos:
+        feromonio[(ponto1, ponto2)] *= (1.0 - taxa_evaporacao)
+    for ponto1, ponto2 in melhor_caminho:
+        feromonio[(ponto1, ponto2)] += 1.0 / melhor_custo_global
+
+# Algoritmo de colônia de formigas
+melhor_caminho_global = None
+melhor_custo_global = float('inf')
+
+for _ in range(num_iteracoes):
+    for _ in range(num_formigas):
+        ponto_atual = 'A'      # Inicializa em um dos pontos de entrega
+        visitados = [ponto_atual]         # Lista de pontos visitados
+        caminho = []           # Caminho percorrido pela formiga
+
+        while len(visitados) < len(pontos_entrega):
+            proximo_ponto = escolher_proximo_ponto(ponto_atual, visitados)
+            caminho.append((ponto_atual, proximo_ponto))
+            visitados.append(proximo_ponto)
+            ponto_atual = proximo_ponto
+
+        atualizar_feromonio(caminho, caminho)
         
-        atualizar_feromonios(feromonios, melhor_rota, taxa_evaporacao, Q)
-    
-    return melhor_rota, melhor_distancia
+        custo_caminho = sum([calcular_distancia(pontos_entrega[p1], pontos_entrega[p2]) for p1, p2 in caminho])
 
-# Lista de coordenadas das cidades
-cidades = [(0, 0), (1, 2), (2, 4), (3, 1), (4, 3)]
+        if custo_caminho < melhor_custo_global:
+            melhor_caminho_global = caminho
+            melhor_custo_global = custo_caminho
 
-# Parâmetros do ACO
-num_formigas = 10
-num_iteracoes = 100
-alpha = 1.0
-beta = 1.0
-taxa_evaporacao = 0.5
-Q = 1.0
-
-# Executar o algoritmo ACO
-melhor_rota, melhor_distancia = aco(num_formigas, num_iteracoes, alpha, beta, taxa_evaporacao, Q)
-
-print("Melhor rota encontrada:", melhor_rota)
-print("Melhor distância encontrada:", melhor_distancia)
+# Resultados
+print("Melhor rota encontrada:", [p[0] for p in melhor_caminho_global])
+print("Menor custo:", melhor_custo_global)
